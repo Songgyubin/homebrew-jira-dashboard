9913ec996400f9c4f6d00a3e7d3884196ffdda17
Songgyubin <thd0427__@naver.com>
2026-05-17 16:33:53 +0900

    Use public release artifacts for cask

--- Casks/jira-menubar.rb
+++ Casks/jira-menubar.rb
@@ -5,7 +5,7 @@ cask "jira-menubar" do
   sha256 arm: "9f50b6c5cc49a66a88a42d417c87757208d342360ac50d51eef9161963f32d5c",
          intel: "26b35002ad1a91a66eb8200b8672ce74a6fcaee352582e961d03e3b82f1e274f"
 
-  url "https://github.com/Songgyubin/JiraDashboard/releases/download/v#{version}/Jira-Menubar-#{version}-#{arch}.dmg"
+  url "https://github.com/Songgyubin/jira-menubar-releases/releases/download/v#{version}/Jira-Menubar-#{version}-#{arch}.dmg"
   name "Jira Menubar"
   desc "macOS menu bar Jira ticket dashboard"
   homepage "https://github.com/Songgyubin/JiraDashboard"
